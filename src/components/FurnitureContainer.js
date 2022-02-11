84695984f84e36f371112c08824f433d073e14e8
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-11 14:34:15 +0530

    added basic project structure

---
+++ src/components/FurnitureContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./FurnitureContainer.css";
+
+const FurnitureContainer = () => {
+  return (
+    <div className='furnitureContainer'>FurnitureContainer</div>
+  )
+}
+
+export default FurnitureContainer
---
+++ src/components/HorizontalCarousel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./HorizontalCarousel.css";
+
+const HorizontalCarousel = () => {
+  return (
+    <div className='horizontalCarousel'>
+        HorizontalCarousel
+
+        
+    </div>
+  )
+}
+
+export default HorizontalCarousel
