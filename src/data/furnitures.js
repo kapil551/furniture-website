599629cf170b5178b472ea9dc741d575521e5c02
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-12 00:04:40 +0530

    Refactored the FurnitureContainer

---
+++ src/data/furnitures.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 'lamp',
+        title: 'Wooden table lamp',
+        price: '$2300',
+    },
+    {
+        id: 'shaped-sofa',
+        title: 'Shaped sofa' ,
+        price: '$2899'
+    },
+    {
+        id: 'sofa',
+        title: 'European style sofa' ,
+        price: '$4890'
+    },
+    {
+        id: 'stool',
+        title: 'Long single stool' ,
+        price: '$3499'
+    },
+    {
+        id: 'lamp',
+        title: 'Wooden table lamp',
+        price: '$2300',
+    },
+    {
+        id: 'shaped-sofa',
+        title: 'Shaped sofa' ,
+        price: '$2899'
+    },
+    {
+        id: 'sofa',
+        title: 'European style sofa' ,
+        price: '$4890'
+    },
+    {
+        id: 'stool',
+        title: 'Long single stool' ,
+        price: '$3499'
+    },
+    {
+        id: 'lamp',
+        title: 'Wooden table lamp',
+        price: '$2300',
+    },
+    {
+        id: 'shaped-sofa',
+        title: 'Shaped sofa' ,
+        price: '$2899'
+    },
+]
